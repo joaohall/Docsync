b7bbc96b97aad5db1ff50853ce460110415f4507
joaohall <enderfiremite@gmail.com>
2024-06-29 03:14:21 -0300

    Adição das frameworks necessárias para o projeto

--- api/records/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import MedicalRecord
-from .serializer import MedicalRecordSerializer
-
-class MedicalRecordViewSet(viewsets.ModelViewSet):
-  queryset=MedicalRecord.objects.all()
-  serializer_class = MedicalRecordSerializer
